1bf31ff4fd0334f1648646aab0c3079f8ce80e0c
John Lao <59539676+MrLao9038@users.noreply.github.com>
2020-11-02 20:21:12 -0500

    Add files via upload

---
+++ GuessMyPass.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import random
+
+print("                                ")
+
+print("Let me Guess The Password You")
+
+print("--------------------------------")
+print("8eeee8 8   8 8eeee 8eeee8 8eeee8") 
+print("8    8 8   8 8     8      8     ") 
+print("8      8   8 8eeee 8eeeee 8eeeee") 
+print("8  eee 8   8 88        88     88") 
+print("8    8 8   8 88    e   88 e   88") 
+print("88eee8 88ee8 88eee 8eee88 8eee88") 
+print("--------------------------------")        
+
+print("                                ")
+
+uppercase_letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+lowercase_letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+numbers = ["0","1","2","3","4","5","6","7","8","9"]
+special_characters = ["@","%","+","|","?","!","#","$","^","?",":",",","(",")","{","}","[","]","~","_","-",".","/","\\"]
+
+char_list = uppercase_letters+ lowercase_letters + numbers + special_characters
+
+real_password = str(input("Please Enter Password:  "))
+
+guess_password = ""
+
+while guess_password != real_password:
+    guess_password = random.choices(char_list, k=len(real_password))
+    print("Attempting to Guess Password: " + str(guess_password))
+
+    if guess_password == list(real_password):
+        print("Your Password is: " + "".join(guess_password))
+        break
